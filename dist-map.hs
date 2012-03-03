4486fe328845dc61825a7916a8cf3190ebd2f1cf
Fumiaki Kinoshita <fumiexcel@gmail.com>
2012-03-03 13:15:36 +0900

    misc

--- dist-map.hs
+++ dist-map.hs
@@ -10,54 +10,9 @@ import System.Random
 
 type Mapping = Array Char Int
 
-dom = ('a', 'z')
-cod = (-2, 7)
-oct = 6
-
-selectRanking :: Float -> [a] -> IO a
-selectRanking e xs = (xs!!) . floor . (* fromIntegral (length xs))
-    . exp . (*e) . log
-    <$> randomRIO (0.0, 1.0)
-
-mutate :: Mapping -> IO Mapping
-mutate = (<$> fmap pure point) . (//)
-    where
-        point = (,) <$> randomRIO dom <*> randomRIO cod
-
-crossoverUniform :: (Mapping, Mapping) -> IO (Mapping, Mapping)
-crossoverUniform = elems *** elems >>> uncurry zip
-    >>> map pure >>> zipWith (<*>) (repeat ch)
-    >>> sequence >>> fmap (unzip >>> listArray dom *** listArray dom)
-    where
-        ch = ([id, swap]!!) <$> randomRIO (0, 1)
-
-nextGeneration :: Float -> Float -- crossover rate, mutational rate
-   -> (Mapping -> Float) -- fitness
-   -> ([Mapping] -> IO Mapping) -- selector
-   -> ((Mapping, Mapping) -> IO (Mapping, Mapping)) -- crossover
-   -> (Mapping -> IO Mapping) -- mutation
-   -> [Mapping] -- current generation
-   -> IO [Mapping]
-
-nextGeneration rC rM f fS fC fM p = fmap (sortBy (compare `on` f))
-    $ collect (length p)
-    $ repeat (randomRIO (0.0, 1.0) >>= operate)
-    where
-        operate x
-            | x < rM      = fmap pure $ fS p >>= fM
-            | x < rM + rC = fmap g    $ (,) <$> fS p <*> fS p >>= fC
-            | otherwise   = fmap pure $ fS p
-            where
-                g (a, b) = [a, b]
-        
-        collect 0 _     = return []
-        collect _ []     = return []
-        collect 1 (x:_)  = take 1 <$> x
-        collect n (x:xs) = x >>= \v -> (v ++) <$> collect (n - length v) xs
-
-firstGeneration :: Int -> IO [Mapping]
-firstGeneration n = replicateM n $ listArray dom
-    <$> (rangeSize dom `replicateM` randomRIO cod)
+dom = ('a', 'z') -- アルファベットの範囲
+cod = (-2, 7) -- 音の範囲
+oct = 6 -- 1オクターブの音の数
 
 fitness :: [String] -> Mapping -> Float
 fitness ws m = sum $ map (valueMelody . map (m!)) $ ws
@@ -78,11 +33,62 @@ valueMelody (x:xs) = f xs x (-1) (0, 0, 0)
             | otherwise = base
             where
                 base = -1 + (fromIntegral c / fromIntegral (c + d) - 0.8)
-        
+
 main = do
-    ws <- map (map toLower) <$> lines <$> getContents
-    compute ws !! 50 >>= print . head
+    f <- fitness <$> map (map toLower) <$> lines <$> getContents
+    origin <- sortBy (on compare f) <$> firstGeneration dom cod 256
+    print . head =<< compute f origin !! 50
     where
-        next ws = nextGeneration 0.7 0.01 (fitness ws)
-        compute ws = scanl (>>=) (sortBy (compare `on` fitness ws) <$> firstGeneration 256)
-            $ repeat $ next ws (selectRanking 2) crossoverUniform mutate
+        compute f origin = scanl (>>=) (return origin) $ repeat $ next f
+        next f = nextGeneration 0.7 0.01 f 
+            `id` selectRanking 2 `id` crossoverUniform dom `id` mutate dom cod
+        
+selectRanking :: Float -> [a] -> IO a
+selectRanking e xs = (xs!!) . floor . (* fromIntegral (length xs))
+    . exp . (*e) . log
+    <$> randomRIO (0.0, 1.0)
+
+firstGeneration :: (Ix d, Random d, Random c) => (d,d) -> (c,c)
+    -> Int -> IO [Array d c]
+
+firstGeneration d c n = replicateM n $ listArray d
+    <$> (rangeSize d `replicateM` randomRIO c)
+            
+mutate :: (Ix d, Random d, Random c) => (d,d) -> (c,c)
+    -> Array d c -> IO (Array d c)
+
+mutate d c = (<$> fmap pure point) . (//)
+    where point = (,) <$> randomRIO d <*> randomRIO c
+
+crossoverUniform :: Ix d => (d,d)
+    -> (Array d e, Array d e) -> IO (Array d e, Array d e)
+
+crossoverUniform d = elems *** elems >>> uncurry zip
+    >>> map pure >>> zipWith (<*>) (repeat ch)
+    >>> sequence >>> fmap (unzip >>> listArray d *** listArray d)
+    where
+        ch = ([id, swap]!!) <$> randomRIO (0, 1)
+
+nextGeneration :: (Ix d, Random d, Random c)
+    => Float -> Float -- crossover rate, mutational rate
+   -> Ord a => (Array d c -> a) -- fitness
+   -> ([Array d c] -> IO (Array d c)) -- selector
+   -> ((Array d c, Array d c) -> IO (Array d c, Array d c)) -- crossover
+   -> (Array d c -> IO (Array d c)) -- mutation
+   -> [Array d c] -- current generation
+   -> IO [Array d c]
+
+nextGeneration rC rM f fS fC fM p = fmap (sortBy $ on compare f)
+    $ collect (length p) $ repeat $ randomRIO (0.0, 1.0) >>= operate
+    where
+        operate x
+            | x < rM      = fmap pure $ fS p >>= fM
+            | x < rM + rC = fmap qure $ (,) <$> fS p <*> fS p >>= fC
+            | otherwise   = fmap pure $ fS p
+            where
+                qure (a, b) = [a, b] -- There are no meanings in 'qure'
+        
+        collect 0 _      = return []
+        collect _ []     = return []
+        collect 1 (x:_)  = take 1 <$> x
+        collect n (x:xs) = x >>= \v -> (v ++) <$> collect (n - length v) xs
--- wordtomelody.hs
+++ wordtomelody.hs
@@ -1,7 +1,8 @@
 import Control.Applicative
 import Control.Arrow
+import Control.Monad
+import Data.Array
 import Data.Char
-import qualified Data.Map as M
 
 import Haskore.Melody.Standard as Melody
 import Haskore.Music.GeneralMIDI as MidiMusic
@@ -9,7 +10,12 @@ import qualified Haskore.Music as Music
 
 import Haskore.Interface.MIDI.Render
 
-mapping = M.fromList $ [('a',2),('b',2),('c',-1),('d',-2),('e',-2),('f',3),('g',1),('h',5),('i',-1),('j',4),('k',6),('l',3),('m',-2),('n',0),('o',0),('p',0),('q',-2),('r',-2),('s',-2),('t',2),('u',1),('v',-2),('w',-2),('x',0),('y',7),('z',-2)] -- [('k', -2), ('m', -1), ('e', 0), ('g', 0), ('y', 0), ('c', 1), ('j', 1), ('n', 1), ('r', 1), ('z', 1), ('i', 2), ('o', 2), ('b', 3), ('v', 3), ('d', 4), ('q', 4), ('p', 4), ('t', 4), ('a', 5), ('u', 5), ('f', 6), ('l', 6), ('s', 6), ('w', 6), ('x', 6), ('h', 7)] -- 音の割り当て(一番大事！)
+mapping = array ('a', 'z') [
+    ('a',2),('b',2),('c',-1),('d',-2),('e',-2),('f',3),('g',1)
+    ,('h',5),('i',-1),('j',4),('k',6),('l',3),('m',-2),('n',0)
+    ,('o',0),('p',0),('q',-2),('r',-2),('s',-2),('t',2),('u',1)
+    ,('v',-2),('w',-2),('x',0),('y',7),('z',-2)
+    ]
 
 scale = [c, ef, f, gf, g, bf] -- blues scale
 tone = uncurry (flip id) . (id *** (scale !!)) . flip divMod (length scale)
@@ -23,8 +29,12 @@ fromInterval (x:xs@(x':xs')) -- 音階からノートを生成する
 fromInterval (x:[]) = [tone x en ()]
 fromInterval [] = []
 
-fromWord w = (line $ fromInterval $ map ((mapping M.!) . toLower) w) +:+ enr -- 文字列から旋律を生成する
+fromWord w = (line $ fromInterval $ map ((mapping!) . toLower) w) +:+ enr -- 文字列から旋律を生成する
 
-make = MidiMusic.fromMelodyNullAttr MidiMusic.AcousticGrandPiano . (Music.changeTempo 3) . transpose 12 . line
+make = MidiMusic.fromMelodyNullAttr MidiMusic.AcousticGrandPiano
+    . (Music.changeTempo 3) . transpose 12 . line
 
-main = lines <$> getContents >>= mapM_ (fileFromGeneralMIDIMusic . (++".mid") &&& make . map fromWord . words >>> uncurry id)
+main = lines <$> getContents >>= mapM_ save
+    where
+        save name = fileFromGeneralMIDIMusic (name ++ ".mid")
+            $ make $ map fromWord $ words name
