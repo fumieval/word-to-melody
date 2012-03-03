6aa27cfc15576a557a9f98c7f9ecce48b4370b26
Fumiaki Kinoshita <fumiexcel@gmail.com>
2012-03-03 19:51:23 +0900

    Changes for readability

--- dist-map.hs
+++ dist-map.hs
@@ -9,10 +9,9 @@ import Data.Char (toLower)
 import System.Random
 import System.Environment (getArgs)
 
-import Control.Concurrent (threadDelay)
 import System.IO (stderr)
-import System.Time (getClockTime, diffClockTimes, 
-                    ClockTime, tdYear, tdMonth, tdHour, tdMin, tdSec)
+import System.Time (getClockTime, diffClockTimes, TimeDiff, ClockTime
+                    ,tdYear, tdMonth, tdHour, tdMin, tdSec)
 import Text.Printf (printf, hPrintf)
 
 import qualified Haskore.Basic.Pitch as Pitch
@@ -29,39 +28,41 @@ valueMelody scale (x:xs) = f xs x (-1) (0, 0, 0)
     where
         oct = length scale 
         f (x:xs) p q (b, c, d)
-            | m == 0 = if b >= 1 then 0.0 else f xs x p (b + 1, c, d)
             | m > oct = 0.0
-            | m == 1 = f xs x p (b, c + 1, d)
-            | otherwise = f xs x p (b, c, d + 1)
+            | b >= 1 && m == 0 = 0.0
+            | m == 0    = f' (b + 1, c, d)
+            | m == 1    = f' (b, c + 1, d)
+            | otherwise = f' (b, c, d + 1)
             where
                 m = abs (p - x)
-        f [] p q (b, c, d)
-            | p `mod` oct == 0 && q == p - 1 = base * 2.0
-            | p `mod` oct == 0 = base * 1.5
-            | otherwise = base
+                f' = f xs x p 
+        
+        f [] p q (b, c, d) = case (p `mod` oct == 0, q == p - 1) of
+            (True, True) -> 2.0 * base
+            (True, False) -> 1.5 * base
+            _ -> base
             where
                 base = -1 + (fromIntegral c / fromIntegral (c + d) - 0.8)
 
-main = do
-    args <- getArgs
-    case args of
-         (param : tone : _) -> do
-             (n, g, c, m, e) <- read <$> readFile param
-             (scale, cod) <- read <$> readFile tone
-             samples <- lines <$> getContents
+main = getArgs >>= \args -> case args of
+    (paramFile:toneFile:_) -> do
+        (n, g, c, m, e) <- readIO =<< readFile paramFile
+        (scale, cod)    <- readIO =<< readFile toneFile
+        samples         <- lines <$> getContents
+        
+        let f = fitness scale $ map (map toLower) samples
+        
+        putStrLn $ "Samples:" ++ show (length samples)
+        
+        origin <- sortBy (on compare f) <$> firstGeneration dom cod n
+        progbar <- getProgressBar "Waiting for the convergence:" g
+        result <- head . snd <$> compute f origin cod progbar c m e !! g
+        
+        progbar g
+        print (scale, assocs result)
              
-             let f = fitness scale $ map (map toLower) samples
-             
-             putStrLn $ "Samples:" ++ show (length samples)
-            
-             origin <- sortBy (on compare f) <$> firstGeneration dom cod n
-             progbar <- getProgressBar "Waiting for the convergence:" g
-             result <- head . snd <$> compute f origin cod progbar c m e !! g
-            
-             progbar g
-             putStrLn ""
-             print (scale, assocs result)
-         _ -> print "Usage: dist-map <param> <tone>"
+    _ -> print "Usage: dist-map <param> <tone>"
+    
     where
         compute f origin cod progbar c m e = scanl (>>=) (return (0, origin))
             $ repeat $ progress *** next >>> uncurry (liftA2 (,))
@@ -71,7 +72,7 @@ main = do
                     `id` selectRanking e
                     `id` crossoverUniform dom
                     `id` mutate dom cod
-        
+
 selectRanking :: Float -> [a] -> IO a
 selectRanking e xs = (xs!!) . floor . (* fromIntegral (length xs))
     . exp . (*e) . log
@@ -86,17 +87,16 @@ firstGeneration d c n = replicateM n $ listArray d
 mutate :: (Ix d, Random d, Random c) => (d,d) -> (c,c)
     -> Array d c -> IO (Array d c)
 
-mutate d c = (<$> fmap pure point) . (//)
-    where point = (,) <$> randomRIO d <*> randomRIO c
+mutate dom cod = (<$> fmap pure point) . (//)
+    where point = (,) <$> randomRIO dom <*> randomRIO cod
 
 crossoverUniform :: Ix d => (d,d)
     -> (Array d e, Array d e) -> IO (Array d e, Array d e)
 
-crossoverUniform d = elems *** elems >>> uncurry zip
+crossoverUniform dom = elems *** elems >>> uncurry zip
     >>> map pure >>> zipWith (<*>) (repeat ch)
-    >>> sequence >>> fmap (unzip >>> listArray d *** listArray d)
-    where
-        ch = ([id, swap]!!) <$> randomRIO (0, 1)
+    >>> sequence >>> fmap (unzip >>> listArray dom *** listArray dom)
+    where ch = ([id, swap]!!) <$> randomRIO (0, 1)
 
 nextGeneration :: (Ix d, Random d, Random c)
     => Float -> Float -- crossover rate, mutational rate
@@ -111,42 +111,40 @@ nextGeneration rC rM f fS fC fM p = fmap (sortBy $ on compare f)
     $ collect (length p) $ repeat $ randomRIO (0.0, 1.0) >>= operate
     where
         operate x
-            | x < rM      = fmap pure $ fS p >>= fM
+            | x < rM      = fmap pure $ fS p                  >>= fM
             | x < rM + rC = fmap qure $ (,) <$> fS p <*> fS p >>= fC
             | otherwise   = fmap pure $ fS p
-            where
-                qure (a, b) = [a, b] -- There are no meanings in 'qure'
+            where qure (a, b) = [a, b] -- There are no meanings in 'qure'
         
+        collect :: Int -> [IO [a]] -> IO [a]
         collect 0 _      = return []
         collect _ []     = return []
         collect 1 (x:_)  = take 1 <$> x
         collect n (x:xs) = x >>= \v -> (v ++) <$> collect (n - length v) xs
 
--- http://d.hatena.ne.jp/ha-tan/20061217/1166316640
+getProgressBar :: Integral a => String -> a -> IO (a -> IO ())
+getProgressBar title total = f <$> getClockTime
+    where        
+        f start n
+            | n >= total = display start 1.0 >> putStrLn ""
+            | otherwise = display start $ fromIntegral n / fromIntegral total
 
-getProgressBar :: String -> Int -> IO (Int -> IO ())
-getProgressBar title total = return . f =<< getClockTime
-  where
-    f start n
-      | n > total = f' start 100 >> putStrLn ""
-      | otherwise = f' start $ n * 100 `div` total
+        fmt = "\r%-15s %3d%% |%s| Time: %s"
+        len = 40
 
-    f' :: ClockTime -> Int -> IO ()
-    f' start percent = do
-      now <- getClockTime
-      hPrintf stderr "\r%-15s %3d%% |%s| Time: %s" title percent bar $ time now
-      where
-        bar =
-          let max = 40
-              n   = percent * max `div` 100
-          in (replicate n '#') ++ (replicate (max - n) ' ')
-
-        time :: ClockTime -> String
-        time now =
-          let elapsed = diffClockTimes now start
-              hour    = (tdYear elapsed * 365 * 24) * 
-                          (tdMonth elapsed * 30) * 
-                          (tdHour elapsed)
-              min     = tdMin elapsed
-              sec     = tdSec elapsed
-          in printf "%02d:%02d:%02d" hour min sec
+        display :: RealFrac a => ClockTime -> a -> IO ()
+        display start p = getClockTime
+            >>= hPrintf stderr fmt title (floor $ p * 100 :: Int) bar
+            . showTime . flip diffClockTimes start
+            where
+                bar = let n = floor $ p * fromIntegral len in
+                          replicate n '#' ++ replicate (len - n) ' '
+        
+        showTime :: TimeDiff -> String
+        showTime elapsed = printf "%02d:%02d:%02d" hour minute second
+            where
+                minute  = tdMin elapsed
+                second  = tdSec elapsed
+                hour    = (tdYear elapsed * 365 * 24)
+                            * (tdMonth elapsed * 30)
+                            * (tdHour elapsed)
--- wordtomelody.hs
+++ wordtomelody.hs
@@ -21,26 +21,26 @@ tone scale n = Melody.note' (scale !! i) o
         (o, i) = divMod n $ length scale
 
 fromInterval :: [Pitch.Class] -> [Int] -> Music.T (Melody.Note ())
-fromInterval scale = group >>> map (head &&& length)
-    >>> map (tone scale *** (Duration.en*) . fromIntegral)
-    >>> map (uncurry id) >>> map ($()) >>> Music.line
+fromInterval scale = group
+    >>> map (head &&& length
+             >>> tone scale *** (Duration.en*) . fromIntegral
+             >>> uncurry id >>> ($()))
+    >>> Music.line
 
-main = do
-    args <- getArgs
-    case args of
-         (method:profile:path:_) -> do
-             (tempo, trans) <- read <$> readFile profile
-             (scale, mapping) <- read <$> readFile path
-             phrases <- lines <$> getContents
-             let f = case method of {
-                "playWinNT" -> const $ void . playWinNT;
-                "playLinux" -> const $ void . playLinux;
-                "playAlsa" -> const $ void . playAlsa;
-                "playTimidity" -> const $ void . playTimidity;
-                "save" -> fileFromGeneralMIDIMusic . (++".mid") }
-             forM_ phrases $ \phrase -> f phrase
-                 $ make tempo trans scale mapping phrase
-         _ -> putStrLn "Usage: wordtomelody [playWinNT|playLinux|playAlsa|playTimidity|save] <profile> <mapping>"
+main = getArgs >>= \args -> case args of
+    (method:profile:path:_) -> do
+        (tempo, trans)   <- readIO =<< readFile profile
+        (scale, mapping) <- readIO =<< readFile path
+        phrases          <- lines <$> getContents
+        let f = case method of {
+            "playWinNT"    -> const $ void . playWinNT;
+            "playLinux"    -> const $ void . playLinux;
+            "playAlsa"     -> const $ void . playAlsa;
+            "playTimidity" -> const $ void . playTimidity;
+            "save"         -> fileFromGeneralMIDIMusic . (++".mid") }
+        forM_ phrases $ \phrase -> f phrase
+            $ make tempo trans scale mapping phrase
+    _ -> putStrLn "Usage: wordtomelody [playWinNT|playLinux|playAlsa|playTimidity|save] <profile> <mapping>"
 
 make tempo trans scale mapping = id
     . MidiMusic.fromMelodyNullAttr MidiMusic.AcousticGrandPiano
